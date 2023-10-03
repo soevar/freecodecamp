67ec07368633705447e1ac2071f69d71051b46ab
Bang Pardi <supardi.salmoe@gmail.com>
2023-10-03 10:28:54 +0700

    root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,20 +1,37 @@
+require("dotenv").config({ path: "./.env" });
+const e = require("express");
 let express = require("express");
+const { log } = require("fcc-express-bground");
 let app = express();
 
 //console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
+// log root level
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
 // routing
 app.get("/", function (req, res) {
-  console.info(req.url);
+  //console.info(req.url);
   //res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req, res) => {
+  let data = "Hello json";
+  //console.info(process.env.MESSAGE_STYLE);
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    data = data.toUpperCase();
+  } else {
+    data;
+  }
+
   res.json({
-    message: "Hello json",
+    message: data,
   });
 });
 
