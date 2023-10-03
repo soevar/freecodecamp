0d8206e942908165784cc453437f1bc3f876f239
Bang Pardi <supardi.salmoe@gmail.com>
2023-10-03 10:06:21 +0700

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -12,4 +12,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
+
 module.exports = app;
