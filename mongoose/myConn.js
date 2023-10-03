e32d99cba5e4cb3e7474e3f84186bc197485a407
soevar <supardi.salmoe@gmail.com>
2023-10-03 22:06:39 +0700

    fix conn

---
+++ mongoose/myConn.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+console.log('conn ' + process.env.MONGO_URI);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
