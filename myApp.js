b1aacc1b810b9fa74d409e29814b9c16d7b62d6e
Bang Pardi <supardi.salmoe@gmail.com>
2023-10-03 09:57:41 +0700

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,41 +1,12 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
-console.log("Hello World");
+//console.log("Hello World");
 
+// routing
+app.get("/", function (req, res) {
+  console.info(req.url);
+  res.send("Hello Express");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
